7f084865806b15c160af26382785c993f159c4e0
sariputriani <kissmenasari47@gmail.com>
2025-10-20 13:33:25 +0700

    search pengumuman

--- lib/scr/pengumuman/pengumuman.dart
+++ lib/scr/pengumuman/pengumuman.dart
@@ -14,11 +14,29 @@ class PengumumanPage extends StatefulWidget {
 
 class _PengumumanPageState extends State<PengumumanPage> {
   late Future<List<Pengumuman>> futurePengumuman;
+  List<Pengumuman> allPengumuman = [];
+  List<Pengumuman> filteredPengumuman = [];
+  TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     futurePengumuman = PengumumanService.getSemuaPengumuman();
+    futurePengumuman.then((value) {
+      setState(() {
+        allPengumuman = value;
+        filteredPengumuman = value;
+      });
+    });
+
+    searchController.addListener(() {
+      final query = searchController.text.toLowerCase();
+      setState(() {
+        filteredPengumuman = allPengumuman.where((p) {
+          return p.judul.toLowerCase().contains(query);
+        }).toList();
+      });
+    });
   }
 
   @override
@@ -35,6 +53,7 @@ class _PengumumanPageState extends State<PengumumanPage> {
         child: Column(
           children: [
             TextField(
+              controller: searchController,
               decoration: InputDecoration(
                 prefixIcon: const Icon(Icons.search),
                 hintText: 'Search',
@@ -45,99 +64,76 @@ class _PengumumanPageState extends State<PengumumanPage> {
             ),
             const SizedBox(height: 10),
             Expanded(
-              child: FutureBuilder<List<Pengumuman>>(
-                future: futurePengumuman,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text("Error: ${snapshot.error}"));
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return const Center(child: Text("Belum ada pengumuman"));
-                  }
-
-                  final data = snapshot.data!;
-                  return ListView.builder(
-                    itemCount: data.length,
-                    itemBuilder: (context, index) {
-                      final item = data[index];
-                      return InkWell(
-                        onTap: () {
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //     builder: (context) => DetailPengumumanPage(
-                          //       judul: item.judul,
-                          //       tanggal: item.tanggal,
-                          //       deskripsi: item.isi,
-                          //       gambar: item.gambar,
-                          //     ),
-                          //   ),
-                          // );
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  DetailPengumumanPage(item: item),
+              child: filteredPengumuman.isEmpty
+                  ? const Center(
+                      child: Text("Kata Kunci Yang di Cari Tidak ada"))
+                  : ListView.builder(
+                      itemCount: filteredPengumuman.length,
+                      itemBuilder: (context, index) {
+                        final item = filteredPengumuman[index];
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      DetailPengumumanPage(item: item)),
+                            );
+                          },
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
                             ),
-                          );
-                        },
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          margin: const EdgeInsets.only(bottom: 12),
-                          elevation: 2,
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                if (item.gambar.isNotEmpty)
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: Image.memory(
-                                      base64Decode(item.gambar),
-                                      fit: BoxFit.cover,
+                            margin: const EdgeInsets.only(bottom: 12),
+                            elevation: 2,
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  if (item.gambar.isNotEmpty)
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Image.memory(
+                                        base64Decode(item.gambar),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    item.judul,
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
-                                const SizedBox(height: 8),
-                                Text(
-                                  item.judul,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
+                                  const SizedBox(height: 4),
+                                  Row(
+                                    children: [
+                                      const Icon(Icons.calendar_today,
+                                          size: 14, color: Colors.grey),
+                                      const SizedBox(width: 4),
+                                      Text(
+                                        item.tanggal,
+                                        style: const TextStyle(
+                                            fontSize: 12, color: Colors.grey),
+                                      ),
+                                    ],
                                   ),
-                                ),
-                                const SizedBox(height: 4),
-                                Row(
-                                  children: [
-                                    const Icon(Icons.calendar_today,
-                                        size: 14, color: Colors.grey),
-                                    const SizedBox(width: 4),
-                                    Text(
-                                      item.tanggal,
-                                      style: const TextStyle(
-                                          fontSize: 12, color: Colors.grey),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 8),
-                                Text(
-                                  item.isi,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(fontSize: 13),
-                                ),
-                              ],
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    item.isi,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(fontSize: 13),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
+                        );
+                      },
+                    ),
             ),
           ],
         ),
